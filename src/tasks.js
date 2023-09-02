46872363976ccc1c8d3377c141913c2e4c7473de
Flowers <floresfernandezalexis05@gmail.com>
2023-09-01 21:55:42 -0600

    Form
    Created the basic structure of the form

---
+++ src/tasks.js
@@ -0,0 +1,20 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Learn React",
+    description: "Learn how to use ReactJS",
+    done: false,
+  },
+  {
+    id: 1,
+    title: "Learn React",
+    description: "Learn how to use ReactJS",
+    done: false,
+  },
+  {
+    id: 2,
+    title: "Learn React",
+    description: "Learn how to use ReactJS",
+    done: false,
+  },
+];
