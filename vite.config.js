bdaa5f9bfa1cca405e799d714639195067f2d743
Flowers <floresfernandezalexis05@gmail.com>
2023-09-02 18:59:42 -0600

    Changing url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Form-react',
+  base: '/https://github.com/AlexisFlo/Form-react',
 })
